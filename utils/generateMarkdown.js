0f739c8adacb0e2822088683b9891a4b80eecc83
mewmew88 <mehr_hejazi@hotmail.com>
2023-02-20 22:43:16 +0000

    generateMarkdown.js fixed to working

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,40 @@
 // function to generate markdown for README
-function generateDescription (description) {
+function generateDescription(description) {
   return `
   ## Description
   ${description}
-  `;
+ `;
 }
 
 function generateMarkdown(data) {
   return `# ${data.title}
-${data.Description !== "" ? generateDescription(data.Description) : ""}
-`;
+  ${data.description !== '' ? generateDescription(data.description) : ''}
+
+  ## Installation
+  ${data.installation};
+  
+  ## Usage
+  ${data.usage};
+
+  ## License
+  ${data.license};
+
+  ## Contributing
+  ${data.contributing};
+
+  ## Tests
+  ${data.tests};
+  
+  ## Questions
+  ${data.faq };
+
+  ## GitHub 
+  ${data.github};
+
+  ## Email
+  ${data.email }`;
 }
 
 module.exports = generateMarkdown;
+function newFunction() {
+  ;''}
